6cd83d59a67d93be4a7dec002ace82fdbd759807
xavvviier <147688740+xavvviier@users.noreply.github.com>
2023-10-12 01:39:35 -0400

    Add files via upload

---
+++ Da Hood/cframe.lua
@@ -0,0 +1,19 @@
+local Player = game.Players.LocalPlayer
+local Mouse = Player:GetMouse()
+
+Mouse.KeyDown:Connect(function(Key)
+    if Key:lower() == getgenv().Settings.CFrameSettings.Keybind and getgenv().Settings.CFrameSettings.Enabled == true then
+        getgenv().Settings.CFrameSettings.Enabled = false
+    elseif Key:lower() == getgenv().Settings.CFrameSettings.Keybind and getgenv().Settings.CFrameSettings.Enabled == false then
+        getgenv().Settings.CFrameSettings.Enabled = true
+        repeat
+            Player.Character.HumanoidRootPart.CFrame = Player.Character.HumanoidRootPart.CFrame + Player.Character.Humanoid.MoveDirection * getgenv().Settings.CFrameSettings.Interval
+            for _, v in pairs(game.Players.LocalPlayer.Character:GetChildren()) do
+                if v:IsA("Script") and v:FindFirstChild("LocalScript") then
+                    v:Destroy()
+                end
+            end
+            game:GetService("RunService").Stepped:wait()
+        until getgenv().Settings.CFrameSettings.Enabled == false
+    end
+end)
---
+++ Da Hood/main.lua
@@ -0,0 +1,21 @@
+getgenv().Settings = {
+    Notification = true,
+    LockSettings = {
+        ["Enabled"] = true,
+        ["Keybind"] = "q",
+        ["FOV"] = math.huge,
+        ["Dot"] = true
+    },
+    CFrameSettings = {
+        ["Enabled"] = true,
+        ["Keybind"] = "z",
+        ["Interval"] = 1
+    },
+    AntiLockSettings = {
+        ["Enabled"] = true,
+        ["Keybind"] = "x",
+        ["Velocity"] = Vector3.new(200, 700, 200)
+    }
+}
+
+print(grwshokw4x)
